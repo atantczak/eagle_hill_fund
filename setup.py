8d4c0d4d9812f2b2924b69ba64aa59840476394f
atantczak <antczak.andrew59@gmail.com>
2023-12-16 11:12:20 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='eagle_hill_fund',
+    version='0.1.0',  # Added version number
     packages=find_packages(),
     description='All Eagle Hill Fund utilities will be held within this codebase.',
     author='Andrew Antczak',
     author_email='antczak.andrew59@gmail.com',
     url='https://github.com/atantczak/eagle_hill_fund',
 )
+
